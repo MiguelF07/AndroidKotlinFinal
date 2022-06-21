47160f93d80c9a1a5f08f50eff14876f2ac42ffa
Miguel Ferreira <miguel.r.ferreira@ua.pt>
2022-06-21 19:35:50 +0100

    changed app color

--- app/src/main/java/com/example/androidkotlinfinal/ui/map/LocationProvider.kt
+++ app/src/main/java/com/example/androidkotlinfinal/ui/map/LocationProvider.kt
@@ -1,31 +1,77 @@
 package com.example.androidkotlinfinal.ui.map
 
 import android.annotation.SuppressLint
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.SphericalUtil
+import kotlin.math.roundToInt
 
 @SuppressLint("MissingPermission")
 
 class LocationProvider(private val activity: AppCompatActivity) {
 
-    //1
-    private val client
+    private val fusedLocation
             by lazy { LocationServices.getFusedLocationProviderClient(activity) }
 
-    //2
     private val locations = mutableListOf<LatLng>()
 
-    //3
     val liveLocation = MutableLiveData<LatLng>()
+    private var distance = 0;
+    val liveLocations = MutableLiveData<List<LatLng>>()
+    val liveDistance = MutableLiveData<Int>()
 
-    //4
     fun getUserLocation() {
-        client.lastLocation.addOnSuccessListener { location ->
-            val latLng = LatLng(location.latitude, location.longitude)
-            locations.add(latLng)
-            liveLocation.value = latLng
+        fusedLocation.lastLocation.addOnSuccessListener { location ->
+            val locationLatLong = LatLng(location.latitude, location.longitude)
+            locations.add(locationLatLong)
+            liveLocation.value = locationLatLong
+        }
+    }
+
+    fun trackUser() {
+        //1
+        val locationRequest = LocationRequest.create()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 5000
+
+        //2
+        fusedLocation.requestLocationUpdates(locationRequest, locationCallback,
+            Looper.getMainLooper())
+    }
+
+    fun stopTracking() {
+        fusedLocation.removeLocationUpdates(locationCallback)
+        locations.clear()
+        distance = 0
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult) {
+            //1
+            val currentLocation = result.lastLocation
+            val latLng = currentLocation?.let { LatLng(it.latitude, currentLocation.longitude) }
+
+            //2
+            val lastLocation = locations.lastOrNull()
+
+            //3
+            if (lastLocation != null) {
+                distance +=
+                    SphericalUtil.computeDistanceBetween(lastLocation, latLng).roundToInt()
+                liveDistance.value = distance
+            }
+
+            //4
+            if (latLng != null) {
+                locations.add(latLng)
+            }
+            liveLocations.value = locations
         }
     }
 }
--- app/src/main/java/com/example/androidkotlinfinal/ui/map/PermissionsManager.kt
+++ app/src/main/java/com/example/androidkotlinfinal/ui/map/PermissionsManager.kt
@@ -8,20 +8,17 @@ import androidx.lifecycle.MutableLiveData
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
 
-class PermissionsManager(
-    activity: AppCompatActivity,
-    private val locationProvider: LocationProvider) {
+class PermissionsManager(activity: AppCompatActivity,
+    private val locProvider: LocationProvider) {
 
-    //1
-    private val locationPermissionProvider = activity.registerForActivityResult(
-        ActivityResultContracts.RequestPermission()) { granted ->
-        if (granted) {
-            locationProvider.getUserLocation()
+    private val locPermissionProvider = activity.registerForActivityResult(
+        ActivityResultContracts.RequestPermission()) { success ->
+        if (success) {
+            locProvider.getUserLocation()
         }
     }
 
-    //2
     fun requestUserLocation() {
-        locationPermissionProvider.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        locPermissionProvider.launch(Manifest.permission.ACCESS_FINE_LOCATION)
     }
 }
