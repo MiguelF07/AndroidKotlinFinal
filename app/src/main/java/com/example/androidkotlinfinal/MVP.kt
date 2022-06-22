c2b19bbbd37a95348f8e103cae67d23770a3cf87
Fábio Martins <fabio.m@ua.pt>
2022-06-22 01:42:00 +0100

    implemented location visualizer

---
+++ app/src/main/java/com/example/androidkotlinfinal/MVP.kt
@@ -0,0 +1,61 @@
+package com.example.androidkotlinfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import com.example.androidkotlinfinal.ui.map.LocationProvider
+import com.example.androidkotlinfinal.ui.map.PermissionsManager
+import com.google.android.gms.maps.model.LatLng
+
+class MVP {
+
+    data class Ui(
+        val formattedSteps : String,
+        val formattedDistance: String,
+        val currentLocation : LatLng?,
+        val  userPath : List<LatLng>
+        ) {
+        companion object {
+            val EMPTY = Ui(
+                formattedSteps = "0",
+                formattedDistance = "0",
+                currentLocation = null,
+                userPath = emptyList()
+            )
+        }
+    }
+
+    class MapPresenter (private val activity:AppCompatActivity){
+        val ui = MutableLiveData(Ui.EMPTY)
+        private val locationProvider = LocationProvider(activity)
+        private val permissionsManager = PermissionsManager(activity, locationProvider)
+        fun onViewCreated(){
+            locationProvider.liveLocations.observe(activity) { locations ->
+                val current = ui.value
+                ui.value = current?.copy(userPath = locations)
+            }
+            locationProvider.liveLocation.observe(activity) { currentLocation ->
+                val current = ui.value
+                ui.value = current?.copy(currentLocation = currentLocation)
+            }
+            locationProvider.liveDistance.observe(activity) { distance ->
+                val current = ui.value
+                val formattedDistance = activity.getString(R.string.distance_value, distance)
+                ui.value = current?.copy(formattedDistance=formattedDistance)
+            }
+        }
+
+        fun onMapLoaded(){
+            permissionsManager.requestUserLocation()
+        }
+        fun startTracking(){
+            locationProvider.trackUser()
+        }
+        fun stopTracking(){
+            locationProvider.stopTracking()
+        }
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/androidkotlinfinal/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinfinal/MainActivity.kt
@@ -1,28 +1,31 @@
 package com.example.androidkotlinfinal
 
+import android.Manifest.permission_group.ACTIVITY_RECOGNITION
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentTransaction
-import androidx.navigation.Navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
-import androidx.navigation.ui.NavigationUI.setupWithNavController
-import com.example.androidkotlinfinal.R
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.androidkotlinfinal.databinding.ActivityMainBinding
 import com.example.androidkotlinfinal.ui.map.LocationProvider
 import com.example.androidkotlinfinal.ui.map.PermissionsManager
 import com.google.android.gms.maps.*
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity(),OnMapReadyCallback {
+class MainActivity : AppCompatActivity(),OnMapReadyCallback{
 
     var mapFragment: MapFragment? = null
 
+    companion object {
+        private const val REQUEST_CODE_ACTIVITY_RECOGNITION = 2
+    }
+
+    private var presenter = MVP.MapPresenter(this)
     private val locationProvider = LocationProvider(this)
     private val permissionManager = PermissionsManager(this, locationProvider)
 
@@ -53,11 +56,26 @@ class MainActivity : AppCompatActivity(),OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+
+        binding!!.startButton.setOnClickListener {
+            startTracking()
+        }
+        binding!!.endButton.setOnClickListener {
+            stopTracking()
+        }
+        presenter.onViewCreated()
+
     }
+    
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        presenter.ui.observe(this) {ui->
+            updateUi(ui);
+        }
 
+        presenter.onMapLoaded()
         //1
         locationProvider.liveLocation.observe(this) { latLng ->
             map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14f))
@@ -68,4 +86,43 @@ class MainActivity : AppCompatActivity(),OnMapReadyCallback {
 
         map.uiSettings.isZoomControlsEnabled = true
     }
+
+    private fun updateUi(ui:MVP.Ui){
+        if(ui.currentLocation != null && ui.currentLocation != map.cameraPosition.target){
+            map.isMyLocationEnabled = true
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(ui.currentLocation, 14f))
+        }
+        binding?.kilometersNumber?.text = ui.formattedDistance
+        binding?.stepsNumber?.text = ui.formattedSteps
+
+    }
+//    fun setupStepCounterListener() {
+//        // 1
+//        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+//        // 2
+//        val stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+//        // 3
+//        stepCounterSensor ?: return
+//        // 4
+//        sensorManager.registerListener(this@MainActivity, stepCounterSensor, SensorManager.SENSOR_DELAY_FASTEST)
+//    }
+
+    private fun startTracking(){
+        presenter.startTracking()
+
+        if (ContextCompat.checkSelfPermission(this, ACTIVITY_RECOGNITION)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(ACTIVITY_RECOGNITION),
+                99);
+        }
+
+    }
+
+
+    private fun stopTracking(){
+        presenter.stopTracking()
+    }
+
+
 }
