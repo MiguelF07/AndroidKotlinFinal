e16c8516b016529c5fc40b740659ffc0a2d99cd0
Miguel Ferreira <miguel.r.ferreira@ua.pt>
2022-06-22 01:35:23 +0100

    fetching data on profile page

--- app/src/main/java/com/example/androidkotlinfinal/ProfileActivity.kt
+++ app/src/main/java/com/example/androidkotlinfinal/ProfileActivity.kt
@@ -2,18 +2,20 @@ package com.example.androidkotlinfinal
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.*
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 
 class ProfileActivity : AppCompatActivity() {
     val db = Firebase.firestore
+    private var mAuth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -25,6 +27,7 @@ class ProfileActivity : AppCompatActivity() {
             finish()
         }
 
+        mAuth = FirebaseAuth.getInstance()
         val routesButton = findViewById<View>(R.id.routeHistory) as Button
         routesButton.setOnClickListener { view: View? ->
             val intent = Intent(this, HistoryRoutesActivity::class.java)
@@ -33,46 +36,37 @@ class ProfileActivity : AppCompatActivity() {
 
         val kilometersData = findViewById<View>(R.id.kilometersDailyData) as TextView
         val stepsData = findViewById<View>(R.id.stepsDailyData) as TextView
-        val timeData = findViewById<View>(R.id.totalDailyTimeData) as TextView
+        val timeData = findViewById<View>(R.id.totalTimeData) as TextView
 
-        getFromFirebase(object : OnDataReceiveCallback {
-            override fun onDataReceived(kilometers: Double, steps: Double, time: Double) {
-                kilometersData.text = kilometers.toString()
-                stepsData.text = steps.toString()
-                timeData.text = time.toString()
+
+        var firestore: FirebaseFirestore
+        firestore = FirebaseFirestore.getInstance()
+        val docIdRef = mAuth!!.uid?.let { db.collection("users").document(it) }
+        docIdRef!!.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val document = task.result
+                println("PRINTT")
+                println(document)
+                document.data?.get("kilometersDaily")
+                kilometersData.text = document.data?.get("kilometersDaily").toString()
+                stepsData.text = document.data?.get("stepsDaily").toString()
+                timeData.text = document.data?.get("timeDaily").toString()
+
+
+            } else {
+                Log.d(mAuth!!.uid, "Failed with: ", task.exception)
             }
-        })
-    }
-    private fun getFromFirebase(callback: OnDataReceiveCallback) {
-        println("Aqui")
-        val database = FirebaseDatabase.getInstance()
-        println("Aqui2")
-        val ref = database.getReference()
-        println("Aqui3")
-        ref.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                println("Aqui4")
-                println(dataSnapshot.child("/dailyData/2022-06-21/kilometersDaily").getValue())
+        }
 
-                var kilometers = dataSnapshot.child("/dailyData/2022-06-21/kilometersDaily").getValue(Double::class.java)
-                var steps = dataSnapshot.child("/dailyData/2022-06-21/stepsDaily").getValue(Double::class.java)
-                var time = dataSnapshot.child("/dailyData/2022-06-21/time").getValue(Double::class.java)
-                if (kilometers != null) {
-                    println("aqui5")
-                    if (steps != null) {
-                        if (time != null) {
-                            callback.onDataReceived(kilometers,steps,time)
-                        }
-                    }
-                }
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {}
-        })
     }
+
+
 
     interface OnDataReceiveCallback {
         fun onDataReceived(kilometers: Double, steps: Double, time:Double)
     }
+
+
+
 
 }
